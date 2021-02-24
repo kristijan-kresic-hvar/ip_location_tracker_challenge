d535b427c49a788cbbdb8c9df0df88c6f7f14728
kristijan-kresic-dev <kristijan.kresic.dev@gmail.com>
2021-02-24 19:34:16 +0100

    Api security hotfix & added env example file

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_BASE_URL = "https://geo.ipify.org/api/v1?apiKey=at_SxAY28jWdFs58dFaUfTfzOi2AOUpi&ipAddress="
+const API_BASE_URL = `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_IPIFY_API_KEY}&ipAddress=`
 
 const getIPInfo = async (ipAddress) => {
     const result = await axios.get(API_BASE_URL + ipAddress)
