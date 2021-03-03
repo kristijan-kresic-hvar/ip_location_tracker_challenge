e5ca2515e992290513a67c00c2886505a62e8153
kristijan-kresic-dev <kristijan.kresic.dev@gmail.com>
2021-03-03 23:00:28 +0100

    Accessibility fixes, rotation alert finalized

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react'
+
 import './App.css';
 
 import Header from './components/Header/Header'
@@ -6,6 +8,28 @@ import RotateAlert from './components/Alerts/RotateAlert/RotateAlert'
 import ToastAlert from './components/Alerts/ToastAlert/ToastAlert'
 
 function App() {
+
+  const [isLandscape, setIsLandscape] = useState(false)
+
+  const checkIfLandscape = () => {
+    if(window.screen.availWidth > window.screen.availHeight && window.screen.availWidth <= 768) {
+      setIsLandscape(true)
+    } else {
+      setIsLandscape(false)
+    }
+  }
+
+  useEffect(() => {
+
+    // Check for orientation immediataly
+    checkIfLandscape()
+
+    // check for landscape when user resizes their window ( browser )
+    window.addEventListener('resize', () => {
+      checkIfLandscape()
+    })
+  }, [])
+
   return (
     <div className="app">
       <Header />
@@ -13,7 +37,7 @@ function App() {
       {/* Testing state of toast messages */}
       <ToastAlert message="There has been an error" />
       {/* if the screen is rotated in landscape mode */}
-      <RotateAlert />
+      {isLandscape && <RotateAlert />}
     </div>
   );
 }
