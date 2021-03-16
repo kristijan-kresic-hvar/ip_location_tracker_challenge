b7ede2ee0755a5f2199a87e51b52af28fc157a35
kristijan-kresic-dev <kristijan.kresic.dev@gmail.com>
2021-03-16 03:49:00 +0100

    Added Sentry error logging service

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { DataLayer } from './DataLayer'
 import reducer, { initialState } from './reducer'
+
+// Use sentry logger if dns is provided from the .env file
+if(process.env.REACT_APP_SENTRY_DNS) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DNS,
+    integrations: [new Integrations.BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
